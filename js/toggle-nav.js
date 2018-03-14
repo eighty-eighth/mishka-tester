0e08d5e4a9b768934adc3a02adfe7d6dd98297f9
Andrey Semaev <mulanamila@gmail.com>
2018-03-14 23:42:05 +0300

    Добавляет webp, модальное, исправляет форму, svg согласно отступам и подчищает лишние файлы

--- js/toggle-nav.js
+++ js/toggle-nav.js
@@ -1,21 +1,29 @@
 var navMain = document.querySelectorAll('.main-nav');
 var pageHeaderToggle = document.querySelector('.page-header__toggle');
+var toggleOpen = document.querySelector('.page-header__icon-menu-open');
+var toggleClose = document.querySelector('.page-header__icon-menu-close');
 
 for (var i = 0; i < navMain.length; i++) {
   // navMain[i].classList.remove('main-nav--nojs');
-  pageHeaderToggle.classList.add('page-header__toggle--closed');
+  // pageHeaderToggle.classList.add('page-header__toggle--closed');
+  toggleOpen.classList.remove('page-header__icon-menu-open--disabled');
+  toggleClose.classList.add('page-header__icon-menu-close--disabled');
   navMain[i].classList.add('main-nav--closed');
 }
 
 pageHeaderToggle.addEventListener('click', function() {
   for (var j = 0; j < navMain.length; j++) {
     if (navMain[j].classList.contains('main-nav--closed')) {
-      pageHeaderToggle.classList.remove('page-header__toggle--closed');
+      // pageHeaderToggle.classList.remove('page-header__toggle--closed');
+      toggleOpen.classList.add('page-header__icon-menu-open--disabled');
+      toggleClose.classList.remove('page-header__icon-menu-close--disabled');
       navMain[j].classList.remove('main-nav--closed');
       navMain[j].classList.add('main-nav--opened');
     }
     else {
-      pageHeaderToggle.classList.add('page-header__toggle--closed');
+      // pageHeaderToggle.classList.add('page-header__toggle--closed');
+      toggleOpen.classList.remove('page-header__icon-menu-open--disabled');
+      toggleClose.classList.add('page-header__icon-menu-close--disabled');
       navMain[j].classList.add('main-nav--closed');
       navMain[j].classList.remove('main-nav--opened');
     }
